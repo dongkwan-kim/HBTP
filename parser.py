c740eaeb1d0ccc58d046640d3aa16f8a64116159
Dongkwan Kim <todoaskit@gmail.com>
2018-04-11 16:03:38 +0900

    Implement event parser

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ __author__ = 'Dongkwan Kim'
 
 
 import os
+from WriterWrapper import WriterWrapper
 
 
 def tree_dir(twitter_year):
@@ -20,5 +21,45 @@ def get_tree_names(twitter_year):
     return tree_names
 
 
+class Event:
+
+    event_id_counter = 0
+
+    def __init__(self, parent_id, user_id, story_id, time_stamp):
+
+        self.event_id = Event.event_id_counter
+        Event.event_id_counter += 1
+
+        self.parent_id = parent_id
+        self.user_id = user_id
+        self.story_id = story_id
+        self.time_stamp = time_stamp
+
+
+def event_one_line(line, story_id=None):
+    """
+    :param story_id:
+    :param line: str "[str, str, str]->[str, str, str]"
+    :return: Event
+    """
+    [parent, my_self] = [eval(x) for x in line.split('->')]
+    parent_id = parent[0]
+    user_id = my_self[0]
+
+    if not story_id:
+        story_id = parent[1]
+
+    time_stamp = my_self[2]
+
+    return Event(parent_id, user_id, story_id, time_stamp)
+
+
+def event_table():
+    """
+    :return: .csv: [event_id, user_id, parent_id, story_id, time_stamp]
+    """
+    writer = WriterWrapper('event_table')
+
+
 if __name__ == '__main__':
-    print(get_tree_names('twitter16'))
+    pass
