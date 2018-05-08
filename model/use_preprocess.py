e46f4467e71b8c7514147a89f47d40ab6a91fbf1
Dongkwan Kim <todoaskit@gmail.com>
2018-05-08 14:39:37 +0900

    Add use_preprocess for using HBTP/preprocess

---
+++ model/use_preprocess.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def use_preprocess():
+    """
+    Usage:
+
+    import use_preprocess as up
+    up.use_preprocess()
+
+    import (some module from preprocess)
+    """
+    sys.path.append('../preprocess')
