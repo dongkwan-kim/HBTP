19bbd119d9e1bb07ea4f7d2c5ac7c2ae24799615
Dongkwan Kim <todoaskit@gmail.com>
2018-05-17 00:05:37 +0900

    Draw word clouds from file

--- utill/word_cloud.py
+++ utill/word_cloud.py
@@ -22,7 +22,7 @@ CURRENT_DIR = os.path.dirname(__file__)
 
 
 # https://jiffyclub.github.io/palettable/
-def get_color_func(color_scheme=pal.cmocean.sequential.Algae_15):
+def get_color_func(color_scheme=pal.cartocolors.sequential.Peach_6):
 
     def _color_func(word, font_size, position, orientation, random_state=None, **kwargs):
         len_colors = len(color_scheme.colors)
@@ -35,7 +35,7 @@ def get_mask(file_name='circle.png'):
     return np.array(Image.open(os.path.join(CURRENT_DIR, file_name)))
 
 
-def draw_word_cloud(text, color_func):
+def draw_word_cloud(text, color_func, output_file=None):
 
     word_cloud = WordCloud(
         width=600,
@@ -46,6 +46,9 @@ def draw_word_cloud(text, color_func):
 
     word_cloud.generate(text)
     word_cloud.recolor(color_func=color_func, random_state=3)
+    if output_file:
+        os.makedirs('word_cloud_output', exist_ok=True)
+        word_cloud.to_file(os.path.join('word_cloud_output', output_file))
 
     plt.figure()
     plt.imshow(word_cloud, interpolation="bilinear")
@@ -53,5 +56,19 @@ def draw_word_cloud(text, color_func):
     plt.show()
 
 
+def drop_words(text: str, word_to_drop: list):
+    r = text
+    for wd in word_to_drop:
+        r = r.replace(wd, ' ')
+    return r
+
+
+def draw_word_cloud_from_file(file_name, color_func):
+    text_list_from_file = [text.strip() for text in open(file_name, 'r', encoding='utf-8').readlines()]
+    for i, text in enumerate(text_list_from_file):
+        text = drop_words(text, ['will', 'said', '\n'])
+        draw_word_cloud(text, color_func, 'word_cloud_{0}.png'.format(i))
+
+
 if __name__ == '__main__':
-    draw_word_cloud(SAMPLE_TEXT, get_color_func())
+    draw_word_cloud_from_file('selected_story.txt', get_color_func())
