0dd7f85bb2e14f69f7739dc5acde6e1b6a88ffc6
Jooyeon Kim <jyscardioid@gmail.com>
2018-08-01 14:56:49 +0900

    change model.A, model.B to corpus.A, corpus.B (revert)

--- model/model.py
+++ model/model.py
@@ -31,9 +31,6 @@ class BaseModel:
             self.V[k] = (1. / self.n_topic) / np.prod(1. - self.V[:k])
         self.V[self.n_topic - 1] = 1.
 
-        self.A = np.random.gamma(shape=1, scale=1, size=[self.M, self.n_topic])
-        self.B = np.random.gamma(shape=1, scale=1, size=[self.M, self.n_topic])
-
         self.p = self.getP(self.V)
         self.mean = np.zeros(self.n_topic)
         self.gamma = np.random.gamma(shape=1, scale=1, size=[self.n_voca, self.n_topic]) + self.dir_prior
