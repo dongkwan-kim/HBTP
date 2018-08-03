dafe6c66a00d444e074aa05685ad8c233b561adc
Dongkwan Kim <todoaskit@gmail.com>
2018-08-03 18:53:20 +0900

    Add test file for RvNN with HBTP dataset

---
+++ baseline/BU_RvNN_test.py
@@ -0,0 +1,12 @@
+from format_story import *
+from Main_BU_RvNN import *
+
+
+if __name__ == '__main__':
+    stories = get_formatted_stories(data_path='../data')
+    eid_pool = list(stories.story_to_id.keys())
+    run_wrapper(
+        path_root='./Rumor_RvNN',
+        _fold='2',
+        _eid_pool=eid_pool,
+    )
